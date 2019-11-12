58c06f2c9599472fb8ab9f46a0a570ccb0132932
Nicole Adams <nicoleadams.boston@gmail.com>
2019-11-11 19:20:28 -0500

    Project set up

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,21 @@
 // Test away
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+// shows the controls and display
+
+describe('<Dashboard/>', () => {
+    it ('renders correctly', () => {
+        const tree = render
+        .create(<Dashboard/>)
+        .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
+
+
+// describe('<Display/>', () => {
+//     it ('shows the controls and display', () => {
+//         const {getByText, findByText } = {<Display/>};
+//         expect(tree).toMatchSnapshot();
+//     });
+// });
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,20 @@
 // Test away!
+
+
+
+// defaults to unlocked and open
+
+// cannot be closed or opened if it is locked
+
+
+
+
+// displays if gate is open/closed and if it is locked/unlocked
+
+// displays 'Closed' if the closed prop is true and 'Open' if otherwise
+
+// displays 'Locked' if the locked prop is true and 'Unlocked' if otherwise
+
+// when locked or closed use the red-led class
+
+// when unlocked or open use the green-led class
